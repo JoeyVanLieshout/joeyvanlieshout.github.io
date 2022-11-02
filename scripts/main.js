2143757902a69fc656f4407ab51fa39cdfe9a0db
AliceEverglade <joe.van.lieshout@sintlucasedu.nl>
2022-11-02 12:14:42 +0100

    I am so cool, I got it done
    yay me, happy me

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+function main(){
+
+}
+
+function GoToDiscord(){
+    window.open("https://discord.gg/TDwaPBSwGr");
+}
+
+function GoToStarJump(){
+    window.open("https://i.simmer.io/@tomD14/~4ddaa092-be9c-f883-6df1-92157a8004d7");
+}
+
+function GoToArcadeHub(){
+    window.open("https://arcade.drimmle.nl/arcade/");
+}

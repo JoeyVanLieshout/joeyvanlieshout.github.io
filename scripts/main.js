c213fc68bba1e5c7693be605597bfa0b442686fc
JoeyVanLieshout <92016978+JoeyVanLieshout@users.noreply.github.com>
2023-05-04 19:05:33 +0200

    portfolio updated with more projects
    still need to add more images and links that I don't have access to rn

--- scripts/main.js
+++ scripts/main.js
@@ -10,6 +10,18 @@ function GoToStarJump(){
     window.open("https://i.simmer.io/@tomD14/~4ddaa092-be9c-f883-6df1-92157a8004d7");
 }
 
+function GoToAetherArena(){
+    window.open("https://docs.google.com/document/d/1G43XToZ2VJDv2kPE6Y88Qf0RWxWwAs8BcOV_Je4f_Q4/edit?usp=sharing")
+}
+
+function GoToAgrifabulaGitHub(){
+    window.open("https://github.com/AliceEverglade/Libracantus-Agrifabula")
+}
+
+function NotClickable(){
+    window.alert("this does not link to anything as of now")
+}
+
 function GoToArcadeHub(){
     window.open("https://arcade.drimmle.nl/arcade/");
 }
